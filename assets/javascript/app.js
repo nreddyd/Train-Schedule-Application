f59dbc486fcf1c1ad67660ae4ecdd408b0f1e6c1
Neeraja <neerajareddy24@gmail.com>
2018-07-26 10:14:09 -0400

    updated index.html and app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,29 +11,92 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$(".btn").on("click", function(event) {
+$("#addTrain").on("click", function(event) {
   event.preventDefault();
 
   var trainName = $("#trainName")
     .val()
     .trim();
-  var destination = $("#destination")
+  var trainDest = $("#destination")
     .val()
     .trim();
-  var firstTrain = $("#firstTrain")
+  var startTrain = $("#firstTrain")
     .val()
     .trim();
-  var frequency = $("#frequency")
+  var trainFrequency = $("#frequency")
     .val()
     .trim();
+
+  var newTrain = {
+    name: trainName,
+    destination: trainDest,
+    firstTrain: startTrain,
+    frequency: trainFrequency
+  };
+  // console.log(trainName);
+  // console.log(trainDest);
+  // console.log(startTrain);
+  // console.log(trainFrequency);
+  database.ref().push(newTrain);
+  // console.log(newTrain);
+
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#firstTrain").val("");
+  $("#frequency").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+  // console.log(childSnapshot.val());
+  console.log("inside childadded");
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().destination;
+  var firstTrain = childSnapshot.val().firstTrain;
+  var frequency = childSnapshot.val().frequency;
+
+  // Employee Info
   console.log(trainName);
   console.log(destination);
   console.log(firstTrain);
   console.log(frequency);
-  database.ref().set({
-    trainName: trainName,
-    destination: destination,
-    firstTrain: firstTrain,
-    frequency: frequency
-  });
+
+  // Prettify the employee start
+  // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  // var empMonths = moment().diff(moment(empStart, "X"), "months");
+  // console.log(empMonths);
+
+  // Calculate the total billed rate
+  // var empBilled = empMonths * empRate;
+  // console.log(empBilled);
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(firstTrain),
+    $("<td>").text(frequency),
+    $("<td>").text(" ")
+  );
+
+  // console.log(newRow);
+  // Append the new row to the table
+  $("#train-schedule > tbody").append(newRow);
+
+  // $("#train-schedule > tbody").append(
+  //   "<tr><td>" +
+  //     trainName +
+  //     "</td><td>" +
+  //     destination +
+  //     "</td><td class='min'>" +
+  //     frequency +
+  //     "</td><td class='min'>" +
+  //     firstTrain +
+  //     "</td><td class='min'>" +
+  //     firstTrain +
+  //     "</td></tr>"
+  // );
 });
